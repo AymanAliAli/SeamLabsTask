c879b4d3a74600de3d522b0ea415662fc86451e3
Ayman Ali <aymanaliali@ymail.com>
2019-07-08 02:28:10 +0200

    Creating project with bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,14 @@ import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MoviesListComponent } from './movies-list/movies-list.component';
-const routes:Routes = [
- // {path:"",component:MoviesListComponent},
-  {path:"movieslist",component:MoviesListComponent},
- // {path:"**",component:MoviesListComponent}
-];
+
 @NgModule({
   declarations: [
     AppComponent,
-    MoviesListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
